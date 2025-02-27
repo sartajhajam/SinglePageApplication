5e2a69a6a6adb9d65424b1e076ecfeba74e2ab6b
sartaj <hajamsaj@gmail.com>
2025-02-27 17:51:14 +0530

    routing using Link

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Link ,Route} from "react-router-dom";
 function App() {
   return <div>
     <BrowserRouter>
+    <Link to="/">Allen</Link>
+
+    <Link to="/neet/online-coaching-class-11">Class 11</Link>
+    <Link to="/neet/online-coaching-class-12">Class 12</Link>
+    <Link to="/neet/online-coaching-class-13">Class 13</Link>
       <Routes>
         <Route path="neet/online-coaching-class-11" element={<Class11Program />} />
         <Route path="neet/online-coaching-class-12" element={<Class12Program />} />
